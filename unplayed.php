866e7e10ae11b9add47effe38d81e39b0a4c5c05
Nhan Le <nhan@glandoresystems.com>
2013-09-04 19:16:30 +0700

    [Implemented] TimeSheetReport for 2/9

---
+++ unplayed.php
@@ -0,0 +1,137 @@
+<?php
+        $rows[] = array(
+                        "episode_id"=>1,
+                        "episode_name"=> "Hotel California",
+                        "episode_thumbnail"=>"http://media3.ak.yelpcdn.com/bpthumb/JAifIRY9JIV_Z5IrrD102Q/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377525960",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>0,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>20
+                );
+
+        $rows[] = array(
+                        "episode_id"=>2,
+                        "episode_name"=> "Ngay khong binh yen",
+                        "episode_thumbnail"=>"http://media2.ak.yelpcdn.com/bpthumb/LPp9JaaGjXYIhIxIIoBI0A/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377610404",
+                        "episode_duration"=>"1 hour 30 min",
+                        "episode_type"=>1,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>50
+                );
+
+        $rows[] = array(
+                        "episode_id"=>1,
+                        "episode_name"=> "Tinh ca cho em",
+                        "episode_thumbnail"=>"http://media3.ak.yelpcdn.com/bpthumb/Tz0vtUWDjb_u-XkrRbv5EA/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377525960",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>0,
+                        "episode_description"=>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididu ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco labor nisi ut aliquip ex ea commodo consequat.<br/><br/> Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
+                        "episode_played"=>0,
+                        "episode_time_played"=>20
+                );
+
+        $rows[] = array(
+                        "episode_id"=>2,
+                        "episode_name"=> "Thanh pho buon",
+                        "episode_thumbnail"=>"http://media4.ak.yelpcdn.com/bpthumb/wv7XY2ENy4HD6ZufRCnXvA/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377610404",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>1,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>50
+                );
+        $rows[] = array(
+                        "episode_id"=>3,
+                        "episode_name"=> "Sao em no voi lay chong",
+                        "episode_thumbnail"=>"http://media3.ak.yelpcdn.com/bpthumb/JAifIRY9JIV_Z5IrrD102Q/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377525960",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>0,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>20
+                );
+
+        $rows[] = array(
+                        "episode_id"=>4,
+                        "episode_name"=> "Em di chua huong",
+                        "episode_thumbnail"=>"http://media3.ak.yelpcdn.com/bpthumb/K_23zz6O6SeN1gX8Y5h8hA/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377610404",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>1,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>50
+                );
+        $rows[] = array(
+                        "episode_id"=>5,
+                        "episode_name"=> "Nguoi ngoai pho",
+                        "episode_thumbnail"=>"http://media3.ak.yelpcdn.com/bpthumb/K_23zz6O6SeN1gX8Y5h8hA/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377525960",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>0,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>20
+                );
+
+        $rows[] = array(
+                        "episode_id"=>6,
+                        "episode_name"=> "Bai thanh ca buon",
+                        "episode_thumbnail"=>"http://media3.ak.yelpcdn.com/bpthumb/K_23zz6O6SeN1gX8Y5h8hA/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377610404",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>1,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>50
+                );
+
+        $rows[] = array(
+                        "episode_id"=>1,
+                        "episode_name"=> "Noi tinh yeu bat dau",
+                        "episode_thumbnail"=>"http://media2.ak.yelpcdn.com/bpthumb/hBZ6N-xVIR6kWM1JKBR-iw/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377525960",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>0,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>20
+                );
+
+        $rows[] = array(
+                        "episode_id"=>2,
+                        "episode_name"=> "Dem thanh pho day sao",
+                        "episode_thumbnail"=>"http://media2.ak.yelpcdn.com/bpthumb/hBZ6N-xVIR6kWM1JKBR-iw/ms",
+                        "episode_html"=>'',
+                        "episode_date"=>"1377610404",
+                        "episode_duration"=>"1 hour 12 min",
+                        "episode_type"=>1,
+                        "episode_description"=>"hom nay troi dep lam",
+                        "episode_played"=>0,
+                        "episode_time_played"=>50
+                );
+
+$data = array(
+		"total" => 5,
+		"start" =>0,
+		"limit" =>25,
+		"rows" => $rows
+	);
+
+echo json_encode($data);
